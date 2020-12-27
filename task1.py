7151d8a2b9079e3cbf0a51798c1e6dc0442f3b9f
Andrii Kepko <akepko@griddynamics.com>
2020-12-28 01:05:55 +0200

    configurable output

--- task1.py
+++ task1.py
@@ -6,7 +6,7 @@ import pyspark.sql.functions as psf
 
 from etl.purchase_attribution import get_sessions, get_purchase_attribution
 
-def main(click_stream_input, purchases_input, format_):
+def main(click_stream_input, purchases_input, purchase_attribution_output, sessions_output, format_):
     pyspark = SparkSession.builder.master("local[2]").getOrCreate()
 
     options = dict()
@@ -21,9 +21,9 @@ def main(click_stream_input, purchases_input, format_):
     sessions = get_sessions(click_stream)
     purchase_attribution = get_purchase_attribution(click_stream, sessions, purchases)
 
-    for df, timestamp_f, file_name in (
-            (purchase_attribution, 'purchaseTime', 'purchase_attribution'),
-            (sessions, 'sessionStart', 'sessions')
+    for df, timestamp_f, output in (
+            (purchase_attribution, 'purchaseTime', purchase_attribution_output),
+            (sessions, 'sessionStart', sessions_output)
         ):
 
         df.withColumn(
@@ -34,14 +34,21 @@ def main(click_stream_input, purchases_input, format_):
             'day', psf.dayofmonth(timestamp_f)
         ).write.partitionBy(
             'year', 'month', 'day'
-        ).mode("overwrite").parquet(f'warehouse/{file_name}.parquet')
+        ).mode("overwrite").parquet(output)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('click_stream_input', type=str)
     parser.add_argument('purchases_input', type=str)
+    parser.add_argument('purchase_attribution_output', type=str)
+    parser.add_argument('sessions_output', type=str)
     parser.add_argument('format', choices=('parquet', 'tsv'))
 
     args = parser.parse_args()
 
-    main(args.click_stream_input, args.purchases_input, args.format)
+    main(
+        args.click_stream_input,
+        args.purchases_input,
+        args.purchase_attribution_output,
+        args.sessions_output, args.format
+    )
--- task2.py
+++ task2.py
@@ -5,30 +5,25 @@ from pyspark.sql import SparkSession
 
 from etl.stats import get_top_campaigns, get_top_channels
 
-def main(purchase_attribution_input, sessions_input, format_):
+def main(purchase_attribution_input, sessions_input, top_campaigns_output, top_channels_output):
     pyspark = SparkSession.builder.master("local[2]").getOrCreate()
 
-    options = dict()
-    tsv_options = {'sep': r'\t', 'header': True}
-    if format_ == 'tsv':
-        format_ = 'csv'
-        options = {**tsv_options}
-
-    purchase_attribution = pyspark.read.format(format_).load(purchase_attribution_input, **options)
-    sessions = pyspark.read.format(format_).load(sessions_input, **options)  
+    purchase_attribution = pyspark.read.parquet(purchase_attribution_input)
+    sessions = pyspark.read.parquet(sessions_input)  
 
     top_campaigns = get_top_campaigns(pyspark, purchase_attribution)
     top_channels = get_top_channels(pyspark, sessions)
 
-    top_campaigns.show()
-    top_channels.show()
+    top_campaigns.write.mode("overwrite").parquet(top_campaigns_output)
+    top_channels.write.mode("overwrite").parquet(top_channels_output)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('purchase_attribution_input', type=str)
     parser.add_argument('sessions_input', type=str)
-    parser.add_argument('format', choices=('parquet', 'tsv'))
+    parser.add_argument('top_campaigns_output', type=str)
+    parser.add_argument('top_channels_output', type=str)
 
     args = parser.parse_args()
 
-    main(args.purchase_attribution_input, args.sessions_input, args.format)
+    main(args.purchase_attribution_input, args.sessions_input, args.top_campaigns_output, args.top_channels_output)
